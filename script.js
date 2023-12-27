b388544ea5ff1de11e07c618de0c4b6dbbdd5593
JLM21-JinkaLM <111144226+JLM21-JinkaLM@users.noreply.github.com>
2023-12-27 15:27:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var randomNumber1=Math.floor(Math.random()*6)+1;
 
 var randomDiceImage="dice" + randomNumber1 + ".png";
 
-var randomImageSource="images/" + randomDiceImage;
+var randomImageSource=randomDiceImage;
 
 var image1=document.querySelectorAll("img")[0];
 
@@ -14,7 +14,7 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomDiceImage2 = "dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource2 = "images/" + randomDiceImage2; //images/dice1.png - images/dice6.png
+var randomImageSource2 = randomDiceImage2; //images/dice1.png - images/dice6.png
 
 var image2 = document.querySelectorAll("img")[1];
 
